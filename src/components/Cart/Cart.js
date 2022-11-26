9909111fefcd66909c9ebb54f1ec7fc5254585d3
mridulgelal <mridul.gelal@gmail.com>
2022-11-26 17:38:11 +0545

    added new cart model and check dublicate cart amount state

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,16 +1,31 @@
-import classes from "./Cart.module.css";
-import Modal from "../UI/Modal";
 import { useContext } from "react";
+
+import Modal from "../UI/Modal";
+import CartItem from "./CartItems";
+import classes from "./Cart.module.css";
 import CartContext from "../../store/cart-context";
 
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
+
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {};
+
+  const cartItemAddHandler = (item) => {};
+
   const cartItems = (
-    <ul className="['cart-items']">
+    <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
-        <li>{item.name}</li>
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
@@ -18,7 +33,7 @@ const Cart = (props) => {
   return (
     <Modal onClose={props.onClose}>
       {cartItems}
-      <div className="classes.total">
+      <div className={classes.total}>
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
@@ -31,4 +46,5 @@ const Cart = (props) => {
     </Modal>
   );
 };
+
 export default Cart;
